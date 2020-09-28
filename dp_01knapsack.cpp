cd217bf12fa5d91bd5fb8245656ba9944063a110
hugcode <nebula.xy@gmail.com>
2020-09-28 14:29:54 +0800

    Create dp_01knapsack.cpp

---
+++ dp_01knapsack.cpp
@@ -0,0 +1,62 @@
+/* 0-1 knapsack problem
+ * Given weights and values of n items, put these items in a knapsack of capacity c
+ * to get the maximum total value in the knapsack 
+ * dp[i][j] 0<=i<=n, 0<=j<=c 
+ * Init: dp[i][j] = 0                                        if i == 0  (n is zero)
+ *                = 0                                        if j == 0  (c is zero)
+ * Rec:  dp[i][j] = dp[i-1][j]                               if w[i] >  j (can't put item i in a knapsack)
+ *                = max(dp[i-1][j], dp[i-1][j-w[i]] + v[i])  if w[i] <= j (can put item i in a knapsack)
+ * Ans:  dp[n][c]
+ */
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int w[] = {6,3, 4, 9, 8, 10};
+int v[] = {2, 4, 3,10, 8, 5};
+int c = 25;
+int dp[100][100];
+
+int main(void)
+{
+    int i, j, n;
+    
+    n = sizeof(w)/sizeof(int);
+
+    for (i=0; i<=n; i++)
+        dp[i][0] = 0;
+    for (j=0; j<=c; j++)
+        dp[0][j] = 0;
+
+    for (i=1; i<=n; i++) {
+        for (j=1; j<=c; j++) {
+            if (w[i-1] <= j)
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i-1]] + v[i-1]);
+            else
+                dp[i][j] = dp[i-1][j];
+        }    
+    }
+
+    printf("     w  v  c %2d\n", c);
+    for(i=0; i<n; i++)
+        printf(" %2d %2d %2d\n", i+1, w[i], v[i]);
+    cout << endl;
+
+    cout << "   ";
+    for (j=0; j<=c; j++)
+        printf(" %2d", j);
+    cout << endl;
+    for (i=0; i<=n; i++) {
+        printf(" %2d ", i);
+
+        for (j=0; j<=c; j++)
+            printf("%2d ", dp[i][j]);
+        cout << endl;
+    }
+
+    cout << dp[n][c] << endl;
+
+    return 0;
+}
